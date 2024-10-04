0b7d761b6d3d37c759567ce6db775d5fca0cd7a9
tonsaiw <tonsai10538@gmail.com>
2024-10-04 14:00:42 +0700

    create crud of posts

--- backend/server/src/posts/posts.service.ts
+++ backend/server/src/posts/posts.service.ts
@@ -1,18 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import * as postsData from '../mockData/posts.json';
+import {format} from 'date-fns';
 
 @Injectable()
 export class PostsService {
   private posts = postsData;
+  private nextId = this.posts.length + 1;
+  private formatDate(date: Date): string {
+    return format(date, 'yyyy-MM-dd HH:mm:ss');  // Standard SQL format
+  }
+  
   create(createPostDto: CreatePostDto) {
     const newPost = {
-      id: this.posts.length + 1,
+      id: this.nextId,
       ...createPostDto,
-      date: new Date().toISOString(),
+      date: this.formatDate(new Date()),
     };
     this.posts.push(newPost);
+    this.nextId++;
     return newPost;
   }
 
@@ -21,16 +28,24 @@ export class PostsService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} post`;
+    const post = this.posts.find((p) => p.id === id);
+    if (!post) {
+      throw new NotFoundException(`Post with ID ${id} not found`);
+    }
+    return post;
   }
-
   update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+    const post = this.findOne(id);
+    Object.assign(post, updatePostDto);
+    post.datetime = this.formatDate(new Date());
+    return post;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} post`;
+    const postIndex = this.posts.findIndex((p) => p.id === id);
+    if (postIndex === -1) {
+      throw new NotFoundException(`Post with ID ${id} not found`);
+    }
+    this.posts.splice(postIndex, 1);
   }
-
-  // This method is used to get the posts data from the JSON file
 }
