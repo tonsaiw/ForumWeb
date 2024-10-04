f9d74a33ad6891bc6ae046ee7f3bb29f27087dc8
tonsaiw <tonsai10538@gmail.com>
2024-10-04 22:07:52 +0700

    setup app.module ส่วน mongo

--- backend/server/src/app.module.ts
+++ backend/server/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PostsModule],
+  imports: [ ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  })
+    ,MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        user: configService.get<string>('MONGO_USER'),
+        pass: configService.get<string>('MONGO_PASSWORD'),
+        dbName: configService.get<string>('MONGO_DB_NAME'),
+      }),
+    }), PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
