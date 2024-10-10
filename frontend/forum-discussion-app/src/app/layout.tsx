d0fa1cac231ccd0e3d27497906c23dff614b8218
tonsaiw <tonsai10538@gmail.com>
2024-10-10 15:09:47 +0700

    Set Auth to layout

--- frontend/forum-discussion-app/src/app/layout.tsx
+++ frontend/forum-discussion-app/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "./context/AuthContext";
 import "./globals.css";
 import { ReactQueryProvider } from "./ReactQueryProvider";
 
@@ -10,7 +11,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <AuthProvider>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </AuthProvider>
       </body>
     </html>
   );
